fb9357ba489d50bebd58a1f94f899e9b12d4b16c
Laercio Simoes <laercio@hpcbrasil.com>
2024-07-16 19:44:17 -0300

    Add Icons and ThirdWeb

---
+++ frontend/biohacking-healthcare/app/layout.tsx
@@ -0,0 +1,73 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: 'Biohacking Healthcare',
+  description: 'Biohacking Healthcare Frontend Example',
+  metadataBase: new URL('https://healthcare.biohacking.ai'),
+  openGraph: {
+    title: 'Biohacking Healthcare',
+    description: 'Biohacking Healthcare Frontend Example',
+    url: 'https://healthcare.biohacking.ai',
+    siteName: 'Biohacking Healthcare',
+    images: '/biohacking.png',
+  },
+  alternates: {
+    canonical: '/',
+  },
+  applicationName: 'Biohacking Healthcare',
+  referrer: 'origin',
+  keywords: ['Biohacking.ai', 'NFT', 'Laercio Simoes', 'XRP Ledger', 'Blockchain in Rio'],
+  authors: [{ name: 'Laercio Simoes' }],
+  creator: 'Laercio Simoes',
+  publisher: 'Laercio Simoes',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  icons: {
+    icon: [
+      { type: "image/png", sizes: "192x192", url: "/icons/android-icon-192x192.png" },
+      { type: "image/png", sizes: "32x32", url: "/icons/favicon-32x32.png" },
+      { type: "image/png", sizes: "96x96", url: "/icons/favicon-96x96.png" },
+      { type: "image/png", sizes: "16x16", url: "/icons/favicon-16x16.png" }
+    ],
+    shortcut: ['/icons/apple-icon-60x60.png'],
+    apple: [
+      { url: '/apple-icon.png' },
+      { url: '/apple-icon-x3.png', sizes: '180x180', type: 'image/png' },
+      { sizes: "57x57", url: "/icons/apple-icon-57x57.png", type: 'image/png' },
+      { sizes: "60x60", url: "/icons/apple-icon-60x60.png", type: 'image/png' },
+      { sizes: "72x72", url: "/icons/apple-icon-72x72.png", type: 'image/png' },
+      { sizes: "76x76", url: "/icons/apple-icon-76x76.png", type: 'image/png' },
+      { sizes: "114x114", url: "/icons/apple-icon-114x114.png", type: 'image/png' },
+      { sizes: "120x120", url: "/icons/apple-icon-120x120.png", type: 'image/png' },
+      { sizes: "144x144", url: "/icons/apple-icon-144x144.png", type: 'image/png' },
+      { sizes: "152x152", url: "/icons/apple-icon-152x152.png", type: 'image/png' },
+      { sizes: "180x180", url: "/icons/apple-icon-180x180.png", type: 'image/png' },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Biohacking Healthcare Frontend Example',
+    description: 'Biohacking Healthcare Frontend Example',
+    images: ['/biohacking.png'],
+  }
+}
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
