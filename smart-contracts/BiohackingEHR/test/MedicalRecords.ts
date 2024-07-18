ddfaf59c7ffd3e749fdeee5cef779ce33cf6e93d
Laercio Simoes <laercio@hpcbrasil.com>
2024-07-18 20:33:33 -0300

    Unit Test Contract

--- smart-contracts/BiohackingEHR/test/MedicalRecords.ts
+++ smart-contracts/BiohackingEHR/test/MedicalRecords.ts
@@ -1,6 +1,37 @@
 
-import { ethers } from "hardhat";
-import { Contract, Signer } from "ethers";
+import {    time,    loadFixture,} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat"
+
+
+console.log("MedicalRecords test");
+
+describe("MedicalRecords", function () {
+    // We define a fixture to reuse the same setup in every test.
+    // We use loadFixture to run this setup once, snapshot that state,
+    // and reset Hardhat Network to that snapshot in every test.
+    async function deployNftFixture() {
+        // Contracts are deployed using the first signer/account by default
+        const [owner, otherAccount] = await ethers.getSigners();
+
+        const MedicalRecords = await ethers.getContractFactory("MedicalRecords");
+        const medicalRecords = await MedicalRecords.deploy();
+
+        return { medicalRecords, owner, otherAccount };
+    }
+
+    describe("Deployment", function () {
+        it("Should set the righ owner", async function () {
+            const { medicalRecords, owner, otherAccount } = await loadFixture(deployNftFixture);
+
+            expect(await medicalRecords.owner()).to.equal(owner);
+        });
+    });
+
+
+});
+
 
 describe("MedicalRecords", function () {
     let MedicalRecords: Contract;
