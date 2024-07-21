8c5b8e1d4e9e28ce3778cf0885522513552f9079
Laercio Simoes <laercio@hpcbrasil.com>
2024-07-21 19:32:17 -0300

    Build

--- frontend/biohacking-xrpl/app/layout.tsx
+++ frontend/biohacking-xrpl/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThirdwebProvider } from "thirdweb/react";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 
@@ -68,7 +69,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>  <ThirdwebProvider>{children}</ThirdwebProvider></body>
+      <body className={inter.className}>
+        <ThirdwebProvider>
+            {children}
+        </ThirdwebProvider>
+      </body>
     </html>
   );
 }
