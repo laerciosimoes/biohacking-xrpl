a24dd98fe4d1aab955f5a0f2ded819730c6626b3
orientpires <orientpires@gmail.com>
2024-07-21 11:54:01 -0300

    att

--- frontend/biohacking-xrpl/app/client.ts
+++ frontend/biohacking-xrpl/app/client.ts
@@ -1,13 +1,14 @@
 import { createThirdwebClient } from "thirdweb";
 
-// Replace this with your client ID string
-// refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
+// Use a variável de ambiente exposta
+const clientId = process.env.ClientId;
+
+console.log("Client ID:", clientId);
 
 if (!clientId) {
-  throw new Error("No client ID provided");
+    throw new Error("No client ID provided");
 }
 
 export const client = createThirdwebClient({
-  clientId: clientId,
+    clientId: clientId,
 });
--- frontend/biohacking-xrpl/app/page.tsx
+++ frontend/biohacking-xrpl/app/page.tsx
@@ -1,21 +1,19 @@
-
-import Image from "next/image";
-import { ConnectButton } from "thirdweb/react";
-import { client } from "./client";
+'use client';
+import Image from 'next/image';
+import { ConnectButton } from '@thirdweb-dev/react';
+import { client } from './client';
 import Link from 'next/link'; 
-import Footer from "./components/footer";
-import logo from "@public/Logo.png"; // Certifique-se de que o caminho está correto
-import LogoWhite from "@public/LogoWhite.png"; // Certifique-se de que o caminho está correto
-import {darkTheme} from "thirdweb/react"
-import { createWallet } from "thirdweb/wallets";
+import Footer from './components/footer';
+import LogoWhite from '@public/LogoWhite.png';
+import { darkTheme } from '@thirdweb-dev/react';
 
 const customTheme = darkTheme({
   colors: {
-  connectedButtonBg: "black",
+    connectedButtonBg: 'black',
   },
 });
-const wallets = [createWallet("io.metamask")];
-export default async function Home() {
+
+const Home: React.FC = () => {
   return (
     <main>
       <video
@@ -24,8 +22,8 @@ export default async function Home() {
         muted
         className="absolute inset-0 w-full h-full object-cover z-[-1]"
         style={{
-          maxHeight: "100vh",
-          maxWidth: "100vw",
+          maxHeight: '100vh',
+          maxWidth: '100vw',
         }}
       >
         <source src="/page1.mov" type="video/mp4" />
@@ -35,33 +33,34 @@ export default async function Home() {
       <div className="absolute top-4 right-4 z-10">
         <ConnectButton
           client={client}
-          theme={"light"}
-          connectModal={{ size: "wide" }}
+          theme={customTheme}
+          connectModal={{ size: 'wide' }}
         />
       </div>
 
-      {/* Logo no canto superior esquerdo */}
       <div className="absolute top-2 left-4 z-10">
         <Image 
           src={LogoWhite} 
           alt="Logo"
-          width={250} // Ajuste a largura conforme necessário
-          height={250} // Ajuste a altura conforme necessário
+          width={250}
+          height={250}
         />
       </div>
 
       <div className="flex items-center justify-center min-h-screen">
-      <div className="p-6 bg-black bg-opacity-70 rounded-lg shadow-lg max-w-3xl text-left">        <p className="text-white font-aldrich text-2xl mb-4">
+        <div className="p-6 bg-black bg-opacity-70 rounded-lg shadow-lg max-w-3xl text-left">
+          <p className="text-white font-aldrich text-2xl mb-4">
             Desbloqueie o potencial dos seus dados médicos com a combinação poderosa de Blockchain e IA. Cadastre-se agora e aproveite o poder do biohacking!
           </p>
           <Link href="/signup" className="font-aldrich inline-block p-4 text-white text-center rounded-md bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900 hover:from-gray-800 hover:via-gray-600 hover:to-gray-800">
             Cadastro
           </Link>
         </div>
-  
-        </div>
+      </div>
 
       <Footer />
     </main>
   );
-}
+};
+
+export default Home;
