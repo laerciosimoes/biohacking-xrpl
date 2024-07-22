1423e8a9fde15c98bfa4888187acde2d347913f6
orientpires <orientpires@gmail.com>
2024-07-21 23:00:08 -0300

    Update design

--- frontend/biohacking-xrpl/app/page.tsx
+++ frontend/biohacking-xrpl/app/page.tsx
@@ -1,12 +1,11 @@
 'use client';
 import Image from 'next/image';
+import "./globals.css";
 import { ConnectButton } from "thirdweb/react";
 import { client } from './client';
 import Link from 'next/link';
 import Footer from './components/footer';
 import LogoWhite from "@public/logo-white.png";
-
-
 
 export default function Home() {
   return (
@@ -32,7 +31,7 @@ export default function Home() {
         />
       </div>
 
-      <div className="absolute top-2 left-4 z-10">
+      <div className="absolute top-0 left-4 z-10">
         <Image
           src={LogoWhite}
           alt="Logo"
@@ -41,12 +40,12 @@ export default function Home() {
         />
       </div>
       <div className="flex items-center justify-center min-h-screen">
-        <div className="p-6 bg-black bg-opacity-70 rounded-lg shadow-lg max-w-3xl text-left">
-          <p className="text-white font-aldrich text-2xl mb-4">
-            Desbloqueie o potencial dos seus dados médicos com a combinação poderosa de Blockchain e IA. Cadastre-se agora e aproveite o poder do biohacking!
-          </p>
-          <Link href="/signup" className="font-aldrich inline-block p-4 text-white text-center rounded-md bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900 hover:from-gray-800 hover:via-gray-600 hover:to-gray-800">
-            Cadastro
+        <div className="p-6 bg-black bg-opacity-70 rounded-lg shadow-lg max-w-3xl text-center ml-8">
+          <h1 className="text-white font-aldrich text-4xl mb-4">
+            Desbloqueie o potencial dos seus dados médicos com a combinação poderosa de Blockchain e IA.
+          </h1>
+          <Link href="/about" className="font-aldrich inline-block p-4 text-white text-center rounded-md bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900 hover:from-gray-800 hover:via-gray-600 hover:to-gray-800">
+            Saiba Mais
           </Link>
         </div>
       </div>
